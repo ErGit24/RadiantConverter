5e5a85499fe16ec9a3529b050390efc49dd87e18
B l a n k <erol_git@outlook.dk>
2023-11-22 13:37:22 +0100

    Add project files.

---
+++ LabConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+
+namespace LabConsolesssssss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Indtast vinklen i gradder min ven:");
+            if (int.TryParse(ReadLine(), out int gradder))
+            {
+                double radianz = GraderTilRadianz(gradder);
+                WriteLine($"Tak min ven\n{gradder} grader er {radianz:F2} radianz.");
+            }
+            else
+            {
+                WriteLine("HALLO Ugyldig indtast. Indtast et heltal min ven.");
+            }
+        }
+        static double GraderTilRadianz(int gradder)
+        {
+            return gradder * (Math.PI / 180);
+        }
+    }
+}
